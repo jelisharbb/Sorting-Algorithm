0a57e7bb6596890c755c2cc7d9c967bb6e181096
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 22:22:34 +0800

    Printed the numbers per loop as well as the sorted numbers.

--- bubble_sort.py
+++ bubble_sort.py
@@ -2,7 +2,7 @@
 # - compare two adjacent elements
 # - if first > second, swap
 # - if first < second, leave as is
-# - repeat steps excluding the sorted larger elements in the left part of the array
+# - repeat steps excluding the sorted larger elements in the right part of the array
 
 # unsorted list of numbers
 numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
@@ -18,3 +18,7 @@ def bubbleSort(numbers):
                 tempNum = numbers[maxNum] # large number will be temporarily stored in a variable
                 numbers[maxNum] = numbers[maxNum + 1] # the smaller number will be placed in the previous position of larger number
                 numbers[maxNum + 1] = tempNum # the larger number will now be stored in the previous position of the smaller number
+
+        print(numbers) # prints the numbers per loop
+    
+    print(f"\nSorted numbers: {numbers}\n")
