2e145656ccec2621795aa5da64290ce71f15c521
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 19:29:11 +0800

    Nested a for loop that will find the smallest number in the unsorted part of the list.

--- selection_sort.py
+++ selection_sort.py
@@ -10,3 +10,6 @@ print(f"\nUnsorted numbers: {numbers}")
 def selectionSort(numbers):
     for element in range(9): # for loop that will iterate over the numbers
         currentNum = element # current element will be stored in a variable for comparing
+        for minNum in range(element, 10): # nested for loop for iterating again the numbers to be compared with the current number
+            if numbers(minNum) < numbers(currentNum): # if condition to check if the iterated number is less than the current number 
+                currentNum = minNum # the value of the current number will be replaced with the minimum number
