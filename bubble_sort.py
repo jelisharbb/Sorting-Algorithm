cd69b88d80ad9e2ad0c19f3b461bb49755d0291f
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 22:20:15 +0800

    Swapped numbers if the condition was satisfied.

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,6 +9,12 @@ numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
 print(f"\nUnsorted numbers: {numbers}\n")
 
 def bubbleSort(numbers):
+
     for element in range(len(numbers)-1, 0, -1): # external for loop for iterating elements from last to first position
         for maxNum in range(element):
             if numbers[maxNum] > numbers[maxNum + 1]: # if statement for comparing the two adjacent elements
+
+                # swapping of numbers
+                tempNum = numbers[maxNum] # large number will be temporarily stored in a variable
+                numbers[maxNum] = numbers[maxNum + 1] # the smaller number will be placed in the previous position of larger number
+                numbers[maxNum + 1] = tempNum # the larger number will now be stored in the previous position of the smaller number
