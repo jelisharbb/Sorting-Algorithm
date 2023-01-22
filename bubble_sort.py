c0d0ce520d3d7a4357e4dd22992157be90bbe69a
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 21:07:01 +0800

    Defined a list containing ten numbers, and printed it.

---
+++ bubble_sort.py
@@ -0,0 +1,9 @@
+# BUBBLE SORT
+# - compare two adjacent elements
+# - if first > second, swap
+# - if first < second, leave as is
+# - repeat steps excluding the sorted larger elements in the left part of the array
+
+# unsorted list of numbers
+numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
+print(f"\nUnsorted numbers: {numbers}\n")
