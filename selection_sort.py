a8b12d95317a69cfc3f4baff29c0b34194f4bb09
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 20:25:16 +0800

    Fixed an error by changing parentheses with brackets.

--- selection_sort.py
+++ selection_sort.py
@@ -8,10 +8,12 @@ numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
 print(f"\nUnsorted numbers: {numbers}\n")
 
 def selectionSort(numbers):
+
     for element in range(9): # for loop that will iterate over the numbers
         currentNum = element # current element will be stored in a variable for comparing
+        
         for minNum in range(element, 10): # nested for loop for iterating again the numbers to be compared with the current number
-            if numbers(minNum) < numbers(currentNum): # if condition to check if the iterated number is less than the current number 
+            if numbers[minNum] < numbers[currentNum]: # if condition to check if the iterated number is less than the current number 
                 currentNum = minNum # the value of the current number will be replaced with the minimum number
         
         # swapping of numbers
