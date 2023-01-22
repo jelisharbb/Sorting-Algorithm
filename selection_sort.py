14fad263093ef36ca8ce33c5bf728428243e6ce5
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 20:28:57 +0800

    Updated the comments.

--- selection_sort.py
+++ selection_sort.py
@@ -1,7 +1,7 @@
 # SELECTION SORT
 # - find the smallest number
-# - swap
-# - repeat steps excluding the sorted smaller elements in the right
+# - swap with the first element in the unsorted part
+# - repeat
 
 # unsorted array of numbers
 numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
