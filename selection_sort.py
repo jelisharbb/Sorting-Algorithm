f6b1e9a4da9c1a16464aa393696c04280dd9da23
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 19:16:28 +0800

    Implemented a for loop to iterate all numbers.

--- selection_sort.py
+++ selection_sort.py
@@ -3,7 +3,10 @@
 # - swap
 # - repeat steps excluding the sorted smaller elements in the right
 
+# unsorted list of numbers
 numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
 print(f"\nUnsorted numbers: {numbers}")
 
 def selectionSort(numbers):
+    for element in range(9): # for loop that will iterate over the numbers
+        currentNum = element # current element will be stored in a variable for comparing
