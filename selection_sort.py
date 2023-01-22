fe40834f66e0893950902b3e2c3082316b962330
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-22 20:17:54 +0800

    Swapped the numbers by storing them temporarily in a variable.

--- selection_sort.py
+++ selection_sort.py
@@ -3,9 +3,9 @@
 # - swap
 # - repeat steps excluding the sorted smaller elements in the right
 
-# unsorted list of numbers
+# unsorted array of numbers
 numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
-print(f"\nUnsorted numbers: {numbers}")
+print(f"\nUnsorted numbers: {numbers}\n")
 
 def selectionSort(numbers):
     for element in range(9): # for loop that will iterate over the numbers
@@ -13,3 +13,8 @@ def selectionSort(numbers):
         for minNum in range(element, 10): # nested for loop for iterating again the numbers to be compared with the current number
             if numbers(minNum) < numbers(currentNum): # if condition to check if the iterated number is less than the current number 
                 currentNum = minNum # the value of the current number will be replaced with the minimum number
+        
+        # swapping of numbers
+        tempNum = numbers[element]
+        numbers[element] = numbers[currentNum]
+        numbers[currentNum] = tempNum
