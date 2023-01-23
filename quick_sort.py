055195ffc74a23cdf66d634542c85e15949c2cc9
Jelisha Ruth Bugnon <73575184+jelisharbb@users.noreply.github.com>
2023-01-23 13:11:31 +0800

    Defined a list of numbers and printed it.

---
+++ quick_sort.py
@@ -0,0 +1,11 @@
+# QUICK SORT
+# - choose any element as pivot
+# - do 'partition': elements that are less than the pivot should be placed on the left side, while elements greater than the pivot should be placed on the right side
+# - choose pivot element in the left side
+# - do 'partition' again until all elements are sorted
+# - choose pivot element in the right side
+# - do 'partition' again until all elements are sorted
+
+# unsorted list of numbers
+numbers = [57, 81, 45, 44, 82, 76, 26, 48, 67, 12]
+print(f"\nUnsorted numbers: {numbers}\n")
